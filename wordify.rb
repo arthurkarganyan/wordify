d779c3eacac38ae84ac03ee7df9af403c946d440
arthur <arthur.karganyan@gmail.com>
2016-11-25 15:09:58 +0200

    args_explain added

--- wordify.rb
+++ wordify.rb
@@ -2,4 +2,17 @@ require_relative 'lib/wordify'
 require_relative 'env'
 require 'pry'
 
-puts Wordify.run(ARGV[1], ARGV[0])
+def args_explain
+  puts "Usage: "
+  puts "ruby wordify.rb ru 23 # => двадцать три"
+  puts "ruby wordify.rb en 71 # => seventy-one"
+end
+
+def execute
+  return args_explain unless ARGV[0]
+  return args_explain unless ARGV[1]
+  puts Wordify.run(ARGV[1], ARGV[0])
+end
+
+execute
+
